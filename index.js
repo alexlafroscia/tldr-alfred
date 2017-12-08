d23879a50a015dd4b5fdd76fb0e4f4c085997603
Alex LaFroscia <alex@lafroscia.com>
2017-12-07 16:04:51 -0800

    Flesh out the package mechanics

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 'use strict';
-const alfy = require('alfy');
 
-alfy.output([
-  {
-    title: 'Unicorn',
-    subtitle: alfy.input
-  }
-]);
+const alfy = require('alfy');
+const { parse } = require('tldr/lib/parser');
+
+const tokens = alfy.input.split(' ');
+const [command, ...search] = tokens;
+
+const url = `https://api.github.com/repos/tldr-pages/tldr/contents/pages/common/${command}.md?ref=master`;
+
+alfy
+  .fetch(url)
+  .then(({ content, encoding }) => {
+    const markdown = new Buffer(content, encoding).toString('ascii');
+
+    const { examples } = parse(markdown);
+    const filteredExamples = alfy.matches(search.join(' '), examples, 'code');
+
+    alfy.output(
+      filteredExamples.map(example => ({
+        title: example.code,
+        subtitle: example.description,
+        text: {
+          copy: example.code,
+          largetype: markdown
+        }
+      }))
+    );
+  })
+  .catch(error => {
+    if (error.statusCode === 403) {
+      alfy.output({
+        title:
+          "You're probably over your rate limit. Slow down and try again later"
+      });
+    } else if (error.statusCode === 404) {
+      alfy.output({
+        title: 'Command not found'
+      });
+    } else {
+      throw error;
+    }
+  });
--- test.js
+++ test.js
@@ -1,14 +1,128 @@
 import test from 'ava';
 import alfyTest from 'alfy-test';
+import nock from 'nock';
 
-test(async t => {
+const PAYLOAD = {
+  name: 'ember.md',
+  path: 'pages/common/ember.md',
+  sha: '8678ea0d2693ceb08abcf5f3a6d17db5eddd71bb',
+  size: 519,
+  url:
+    'https://api.github.com/repos/tldr-pages/tldr/contents/pages/common/ember.md?ref=master',
+  html_url:
+    'https://github.com/tldr-pages/tldr/blob/master/pages/common/ember.md',
+  git_url:
+    'https://api.github.com/repos/tldr-pages/tldr/git/blobs/8678ea0d2693ceb08abcf5f3a6d17db5eddd71bb',
+  download_url:
+    'https://raw.githubusercontent.com/tldr-pages/tldr/master/pages/common/ember.md',
+  type: 'file',
+  content:
+    'IyBlbWJlcgoKPiBUaGUgRW1iZXIuanMgY29tbWFuZCBsaW5lIHV0aWxpdHku\nCj4gVXNlZCBmb3IgY3JlYXRpbmcgYW5kIG1haW50YWluaW5nIEVtYmVyLmpz\nIGFwcGxpY2F0aW9ucy4KCi0gQ3JlYXRlIGEgbmV3IEVtYmVyIGFwcGxpY2F0\naW9uOgoKYGVtYmVyIG5ldyB7e215X25ld19hcHB9fWAKCi0gQ3JlYXRlIGEg\nbmV3IEVtYmVyIGFkZG9uOgoKYGVtYmVyIGFkZG9uIHt7bXlfbmV3X2FkZG9u\nfX1gCgotIEJ1aWxkIHRoZSBwcm9qZWN0OgoKYGVtYmVyIGJ1aWxkYAoKLSBS\ndW4gdGhlIGRldmVsb3BtZW50IHNlcnZlcjoKCmBlbWJlciBzZXJ2ZWAKCi0g\nUnVuIHRoZSB0ZXN0IHN1aXRlOgoKYGVtYmVyIHRlc3RgCgotIFJ1biBhIGJs\ndWVwcmludCB0byBnZW5lcmF0ZSBzb21ldGhpbmcgbGlrZSBhIHJvdXRlIG9y\nIGNvbXBvbmVudDoKCmBlbWJlciBnZW5lcmF0ZSB7e3R5cGV9fSB7e25hbWV9\nfWAKCi0gSW5zdGFsbCBhbiBlbWJlci1jbGkgYWRkb246CgpgZW1iZXIgaW5z\ndGFsbCB7e25hbWVfb2ZfYWRkb259fWAK\n',
+  encoding: 'base64',
+  _links: {
+    self:
+      'https://api.github.com/repos/tldr-pages/tldr/contents/pages/common/ember.md?ref=master',
+    git:
+      'https://api.github.com/repos/tldr-pages/tldr/git/blobs/8678ea0d2693ceb08abcf5f3a6d17db5eddd71bb',
+    html: 'https://github.com/tldr-pages/tldr/blob/master/pages/common/ember.md'
+  }
+};
+const largetype =
+  '# ember\n\n> The Ember.js command line utility.\n> Used for creating and maintaining Ember.js applications.\n\n- Create a new Ember application:\n\n`ember new {{my_new_app}}`\n\n- Create a new Ember addon:\n\n`ember addon {{my_new_addon}}`\n\n- Build the project:\n\n`ember build`\n\n- Run the development server:\n\n`ember serve`\n\n- Run the test suite:\n\n`ember test`\n\n- Run a blueprint to generate something like a route or component:\n\n`ember generate {{type}} {{name}}`\n\n- Install an ember-cli addon:\n\n`ember install {{name_of_addon}}`\n';
+
+test('it can fetch all of the examples for a command', async t => {
+  nock('https://api.github.com')
+    .get('/repos/tldr-pages/tldr/contents/pages/common/ember.md?ref=master')
+    .reply(200, PAYLOAD);
+
   const alfy = alfyTest();
-  const result = await alfy('Rainbow');
+  const result = await alfy('ember');
 
   t.deepEqual(result, [
     {
-      title: 'Unicorn',
-      subtitle: 'Rainbow'
+      title: 'ember new {{my_new_app}}',
+      subtitle: 'Create a new Ember application:',
+      text: {
+        copy: 'ember new {{my_new_app}}',
+        largetype
+      }
+    },
+    {
+      title: 'ember addon {{my_new_addon}}',
+      subtitle: 'Create a new Ember addon:',
+      text: {
+        copy: 'ember addon {{my_new_addon}}',
+        largetype
+      }
+    },
+    {
+      title: 'ember build',
+      subtitle: 'Build the project:',
+      text: {
+        copy: 'ember build',
+        largetype
+      }
+    },
+    {
+      title: 'ember serve',
+      subtitle: 'Run the development server:',
+      text: {
+        copy: 'ember serve',
+        largetype
+      }
+    },
+    {
+      title: 'ember test',
+      subtitle: 'Run the test suite:',
+      text: {
+        copy: 'ember test',
+        largetype
+      }
+    },
+    {
+      title: 'ember generate {{type}} {{name}}',
+      subtitle:
+        'Run a blueprint to generate something like a route or component:',
+      text: {
+        copy: 'ember generate {{type}} {{name}}',
+        largetype
+      }
+    },
+    {
+      title: 'ember install {{name_of_addon}}',
+      subtitle: 'Install an ember-cli addon:',
+      text: {
+        copy: 'ember install {{name_of_addon}}',
+        largetype
+      }
+    }
+  ]);
+});
+
+test('it can filter the examples for a command', async t => {
+  nock('https://api.github.com')
+    .get('/repos/tldr-pages/tldr/contents/pages/common/ember.md?ref=master')
+    .reply(200, PAYLOAD);
+
+  const alfy = alfyTest();
+  const result = await alfy('ember new');
+
+  t.deepEqual(result, [
+    {
+      title: 'ember new {{my_new_app}}',
+      subtitle: 'Create a new Ember application:',
+      text: {
+        copy: 'ember new {{my_new_app}}',
+        largetype
+      }
+    },
+    {
+      title: 'ember addon {{my_new_addon}}',
+      subtitle: 'Create a new Ember addon:',
+      text: {
+        copy: 'ember addon {{my_new_addon}}',
+        largetype
+      }
     }
   ]);
 });
