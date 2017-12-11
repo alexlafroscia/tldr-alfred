89a3888a229500d4fb3b7250971500edab98e02a
Alex LaFroscia <alex@lafroscia.com>
2017-12-11 15:59:29 -0800

    Add search OSX docs as well as the common set

--- index.js
+++ index.js
@@ -15,49 +15,100 @@ if (require.main === module) {
   }).then(alfy.output);
 }
 
+function allSettled(promises) {
+  const wrappedPromises = promises.map(p =>
+    Promise.resolve(p).then(
+      val => ({ state: 'fulfilled', value: val }),
+      err => ({ state: 'rejected', reason: err })
+    )
+  );
+
+  return Promise.all(wrappedPromises);
+}
+
+/**
+ * Hit the Github API to fetch results for the query
+ *
+ * @param {string} url
+ * @param {object} options
+ * @return {Promise} filtered results
+ */
+function fetchResults(url, options) {
+  return alfy.fetch(url, options).then(payload => {
+    const { content, encoding } = payload;
+    const markdown = new Buffer(content, encoding).toString('ascii');
+    const { name, examples } = parse(markdown);
+
+    return examples.map(example => ({
+      title: example.code,
+      subtitle: example.description,
+      quicklookurl: `https://tldr.ostera.io/${name}`,
+      text: {
+        copy: example.code,
+        largetype: markdown
+      }
+    }));
+  });
+}
+
+/**
+ * Make the URL for the Github API based on the type of docs to search and the command
+ *
+ * @param {string} type the docset to search
+ * @param {string} command the command to search for
+ * @return {string} the API URL
+ */
+function buildURL(type, command) {
+  return `https://api.github.com/repos/tldr-pages/tldr/contents/pages/${type}/${command}.md?ref=master`;
+}
+
 function main(input, { username, password } = {}) {
   const tokens = input.split(' ');
   const [command, ...search] = tokens;
-  const url = `https://api.github.com/repos/tldr-pages/tldr/contents/pages/common/${command}.md?ref=master`;
+  const searchCommand = search.join(' ');
 
   const hasAuthentication = username && password;
+  const options = {
+    maxAge: IS_TESTING ? undefined : CACHE_AGE,
+    auth: hasAuthentication ? `${username}:${password}` : undefined
+  };
 
-  return alfy
-    .fetch(url, {
-      maxAge: IS_TESTING ? undefined : CACHE_AGE,
-      auth: hasAuthentication ? `${username}:${password}` : undefined
-    })
-    .then(payload => {
-      const { content, encoding } = payload;
-      const markdown = new Buffer(content, encoding).toString('ascii');
+  return allSettled([
+    fetchResults(buildURL('common', command), options),
+    fetchResults(buildURL('osx', command), options)
+  ]).then(results => {
+    const successfulResults = results
+      .filter(result => result.state === 'fulfilled')
+      .map(result => result.value);
 
-      const { name, examples } = parse(markdown);
-      const filteredExamples = alfy.matches(search.join(' '), examples, 'code');
+    if (successfulResults.length > 0) {
+      return successfulResults
+        .reduce((acc, results) => [...acc, ...results], [])
+        .filter(result => result.title.includes(searchCommand));
+    }
 
-      return filteredExamples.map(example => ({
-        title: example.code,
-        subtitle: example.description,
-        quicklookurl: `https://tldr.ostera.io/${name}`,
-        text: {
-          copy: example.code,
-          largetype: markdown
-        }
-      }));
-    })
-    .catch(error => {
-      if (error.statusCode === 403) {
-        return {
+    const errors = results
+      .filter(result => result.state === 'rejected')
+      .map(result => result.reason);
+    const [error] = errors;
+
+    if (error.statusCode === 403) {
+      return [
+        {
           title:
             "You're probably over your rate limit. Slow down and try again later"
-        };
-      } else if (error.statusCode === 404) {
-        return {
+        }
+      ];
+    } else if (error.statusCode === 404) {
+      return [
+        {
           title: 'Command not found'
-        };
-      } else {
-        throw error;
-      }
-    });
+        }
+      ];
+    } else {
+      throw error;
+    }
+  });
 }
 
 module.exports = main;
--- test/workflow-test.js
+++ test/workflow-test.js
@@ -15,7 +15,9 @@ test.afterEach(() => {
 test('it can fetch all of the examples for a command', async t => {
   nock('https://api.github.com')
     .get('/repos/tldr-pages/tldr/contents/pages/common/ember.md?ref=master')
-    .reply(200, payload);
+    .reply(200, payload)
+    .get('/repos/tldr-pages/tldr/contents/pages/osx/ember.md?ref=master')
+    .reply(404);
 
   const result = await run('ember');
 
@@ -90,7 +92,9 @@ test('it can fetch all of the examples for a command', async t => {
 test('it can filter the examples for a command', async t => {
   nock('https://api.github.com')
     .get('/repos/tldr-pages/tldr/contents/pages/common/ember.md?ref=master')
-    .reply(200, payload);
+    .reply(200, payload)
+    .get('/repos/tldr-pages/tldr/contents/pages/osx/ember.md?ref=master')
+    .reply(404);
 
   const result = await run('ember new');
 
@@ -133,10 +137,117 @@ test('it can send credentials to the Github server', async t => {
       t.is(password, 'bar');
 
       return payload;
-    });
+    })
+    .get('/repos/tldr-pages/tldr/contents/pages/osx/ember.md?ref=master')
+    .reply(404);
 
   await run('ember', {
     username: 'foo',
     password: 'bar'
   });
+});
+
+test('it can get results from the OSX set of commands', async t => {
+  nock('https://api.github.com')
+    .get('/repos/tldr-pages/tldr/contents/pages/common/ember.md?ref=master')
+    .reply(404)
+    .get('/repos/tldr-pages/tldr/contents/pages/osx/ember.md?ref=master')
+    .reply(200, payload);
+
+  const result = await run('ember');
+
+  t.deepEqual(result, [
+    {
+      title: 'ember new {{my_new_app}}',
+      subtitle: 'Create a new Ember application:',
+      quicklookurl,
+      text: {
+        copy: 'ember new {{my_new_app}}',
+        largetype
+      }
+    },
+    {
+      title: 'ember addon {{my_new_addon}}',
+      subtitle: 'Create a new Ember addon:',
+      quicklookurl,
+      text: {
+        copy: 'ember addon {{my_new_addon}}',
+        largetype
+      }
+    },
+    {
+      title: 'ember build',
+      subtitle: 'Build the project:',
+      quicklookurl,
+      text: {
+        copy: 'ember build',
+        largetype
+      }
+    },
+    {
+      title: 'ember serve',
+      subtitle: 'Run the development server:',
+      quicklookurl,
+      text: {
+        copy: 'ember serve',
+        largetype
+      }
+    },
+    {
+      title: 'ember test',
+      subtitle: 'Run the test suite:',
+      quicklookurl,
+      text: {
+        copy: 'ember test',
+        largetype
+      }
+    },
+    {
+      title: 'ember generate {{type}} {{name}}',
+      subtitle:
+        'Run a blueprint to generate something like a route or component:',
+      quicklookurl,
+      text: {
+        copy: 'ember generate {{type}} {{name}}',
+        largetype
+      }
+    },
+    {
+      title: 'ember install {{name_of_addon}}',
+      subtitle: 'Install an ember-cli addon:',
+      quicklookurl,
+      text: {
+        copy: 'ember install {{name_of_addon}}',
+        largetype
+      }
+    }
+  ]);
+});
+
+test('it can show results from both the common and OSX set', async t => {
+  nock('https://api.github.com')
+    .get('/repos/tldr-pages/tldr/contents/pages/osx/ember.md?ref=master')
+    .reply(200, payload)
+    .get('/repos/tldr-pages/tldr/contents/pages/common/ember.md?ref=master')
+    .reply(200, payload);
+
+  const result = await run('ember');
+
+  t.is(result.length, 14);
+});
+
+test('it shows a message when there is no matching command', async t => {
+  nock('https://api.github.com')
+    .get('/repos/tldr-pages/tldr/contents/pages/osx/ember.md?ref=master')
+    .reply(404)
+    .get('/repos/tldr-pages/tldr/contents/pages/common/ember.md?ref=master')
+    .reply(404);
+
+  const result = await run('ember');
+
+  t.deepEqual(result, [
+    {
+      title: 'Command not found'
+    }
+  ]);
 });
